577def99fa2db2f45a8492c78c91047a97c6de48
1337_fenix <borgeringstad@Brge-Ringstad-PC.local>
2022-08-19 12:44:07 -0400

    fixed some logic in views/password

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Password Protected Web App
 Credits: Web Dev Simplified Channel
 Password protected file web app. (MongoDB, Multer and Express)
+
+Software used:
+MongoDB Compass 
+
+Little styles and design, project is mainly about logic.
+Passcode protected files. (Can't download a file without a required passcode
+Download count. (A file got 5 downloads, another file got 10 downloads)
+Upload one file at a time.
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const File = require('./models/File');
 const app = express();
 const upload = multer({dest: 'uploads' });
-
+app.use(express.urlencoded({ extended: true }))
 mongoose.connect(process.env.DATABASE_URL);
 app.set("view engine", "ejs")
 
@@ -26,14 +26,29 @@ app.post("/upload", upload.single('file'), async (req, res) => {
     res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
 })
 
-app.get("/file/:id", async (req, res) => {
+app.route("/file/:id").get(handleDownload).post(handleDownload);
+// app.get("/file/:id", handleDownload);
+// app.post("/file/:id", handleDownload);
+
+async function handleDownload(req, res) {
     const file = await File.findById(req.params.id);
+    
+    if(file.password != null) {
+        if(req.body.password == null) {
+            res.render("password")
+            return
+        }
+        if(!(await bcrypt.compare(req.body.password, file.password))) {
+            res.render("password", { error: true })
+            return
+        }
+    }
 
     file.downloadCount++
     await file.save()
     console.log(file.downloadCount);
 
     res.download(file.path, file.originalName);
-})
+}
 
 app.listen(process.env.PORT );
